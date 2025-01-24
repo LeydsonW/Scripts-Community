d601868a419d60ef36e2b4f547ebdb820f93dd8e
LeydsonW <120534903+LeydsonW@users.noreply.github.com>
2025-01-23 21:40:12 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local urlScript = 'https://raw.githubusercontent.com/mrlthebest/Community-Scripts/main/community_scripts.lua';
+local urlScript = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/community_scripts.lua';
 modules.corelib.HTTP.get(urlScript, function(script) 
     assert(loadstring(script))() 
 end);
