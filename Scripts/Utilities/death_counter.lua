05ebaeb790c96cf6310a1c5f59626d1d5cae6f7a
LeydsonW <leydsonwallyson@gmail.com>
2025-01-25 13:30:54 -0300

    Update death_counter.lua

--- Scripts/Utilities/death_counter.lua
+++ Scripts/Utilities/death_counter.lua
@@ -4,11 +4,11 @@ setDefaultTab('Utils');
 
 UI.TextEdit(storage.logoutDeaths or "5", function(widget, text)
   storage.logoutDeaths = tonumber(text);
-end, tabName);
+end, 'Utils');
 if type(storage["death"]) ~= "table" then storage["death"] = { count = 0 } end
 local deathCount = storage["death"].count
-UI.Separator(tabName)
-deathLabel = UI.Label("Death count: " .. deathCount, tabName)
+UI.Separator('Utils')
+deathLabel = UI.Label("Death count: " .. deathCount, 'Utils')
 
 if deathCount >= storage.logoutDeaths then
   CaveBot:setOff()
@@ -30,9 +30,9 @@ UI.Button("Reset Deaths", function()
   storage["death"].count = 0
   deathLabel:setText("Death count: " .. storage["death"].count)
   deathLabel:setColor("green")
-end, tabName)
+end, 'Utils')
 
-local macroDeathCount = macro(macroDelay, macroName, function() end, tabName)
+local macroDeathCount = macro(macroDelay, macroName, function() end, 'Utils')
 
 onTextMessage(function(mode, text)
   if macroDeathCount.isOff() then return end
