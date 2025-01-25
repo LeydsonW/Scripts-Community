8cc44423b646e454e233e3a14bcdee0099591ffa
LeydsonW <leydsonwallyson@gmail.com>
2025-01-25 00:05:57 -0300

    Update open_main_bp.lua

--- Scripts/Utilities/open_main_bp.lua
+++ Scripts/Utilities/open_main_bp.lua
@@ -1,11 +1,10 @@
 local macroName = 'Keep Open Main BP';
 local macroDelay = 100;
-setDefaultTab('Utils');
+-- setDefaultTab('Utils');
 
 macro(macroDelay, macroName, function()
     --Made By VivoDibra#1182
     if not getContainers()[0] and getBack() then
         g_game.open(getBack())
     end
-setDefaultTab('Utils')
-end)
+end, setDefaultTab('Utils'))
