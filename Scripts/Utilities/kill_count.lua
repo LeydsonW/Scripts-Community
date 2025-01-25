451f0711b346ffb84a47ba295cdf1796f43e3caa
LeydsonW <leydsonwallyson@gmail.com>
2025-01-25 13:40:30 -0300

    Update kill_count.lua

--- Scripts/Utilities/kill_count.lua
+++ Scripts/Utilities/kill_count.lua
@@ -1,12 +1,8 @@
---[[
-  Script made by Lee (Discord: l33_) - www.trainorcreations.com
-  If you want to support my work, feel free to donate at https://trainorcreations.com/donate
-  PS. Stop ripping off my work and selling it as your own.
-]]--
-local macroName = 'Monster Kill';
-local macroDelay = 100;
 setDefaultTab("Main");
 addSeparator()
+
+local macroName = 'Monster Kill';
+local macroDelay = 100;
 local mkPanelname = "monsterKill"
 if not storage[mkPanelname] then storage[mkPanelname] = { min = false } end
 
