e79b348ead05d61f539cb04d4089fb6b0db218ea
LeydsonW <leydsonwallyson@gmail.com>
2025-01-24 23:31:27 -0300

    Update use_nearby_door.lua

--- Scripts/Utilities/use_nearby_door.lua
+++ Scripts/Utilities/use_nearby_door.lua
@@ -25,13 +25,13 @@ local function properTable(t)
   return r
 end
 
-UI.Separator(Utils)
-UI.Label("Door IDs", Utils)
+UI.Separator()
+UI.Label("Door IDs")
 
 local doorContainer = UI.Container(function(widget, items)
   storage.doorIds = items
   doorId = properTable(storage.doorIds)
-end, Utils)
+end)
 
 doorContainer:setHeight(35)
 doorContainer:setItems(storage.doorIds)
@@ -56,5 +56,5 @@ clickDoor = macro(macroDelay, macroName, function()
           end
       end
   end
-end, Utils)
-UI.Separator(Utils)
+end)
+UI.Separator()
