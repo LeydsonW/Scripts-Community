bbb15c3001419c4cb4ba8800744a012e03a12aef
LeydsonW <leydsonwallyson@gmail.com>
2025-01-25 12:51:10 -0300

    1.0

--- script_list.lua
+++ script_list.lua
@@ -5,84 +5,14 @@ script_manager = {
     _cache = {
 
         Nto = {
-            ['Bug Map Kunai'] = {
-                url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Nto/bug_map_kunai.lua',
-                description = 'Script de bug map kunai para pc.',
-                author = 'Ryan',
-                enabled = false
-            },
-            ['Bug Map Kunai Mobile'] = {
-                url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Nto/bug_map_mobile_kunai.lua',
-                description = 'Script de bug map kunai para mobile.',
-                author = 'VictorNeox',
-                enabled = false
-            },
             ['Stack'] = {
                 url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Nto/stack.lua',
                 description = 'Script de stack, vai soltar a spell no monstro mais distante da tela.',
-                author = 'VictorNeox',
+                author = 'Ley',
                 enabled = false
             },
 
         },
-        -- Tibia = {
-
-        --     ['Auto Mount'] = {
-        --         url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Tibia/auto_mount.lua',
-        --         description = 'Script de montagem automatica.',
-        --         author = 'VivoDibra',
-        --         enabled = false
-        --     },
-        --     ['Cast Food'] = {
-        --         url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Tibia/cast_food.lua',
-        --         description = 'Script de castar/usar a food.',
-        --         author = 'VivoDibra',
-        --         enabled = false
-        --     },
-        --     ['E Ring'] = {
-        --         url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Tibia/e_ring.lua',
-        --         description = 'Script de e-ring.',
-        --         author = 'VivoDibra',
-        --         enabled = false
-        --     },
-        --     ['Exeta Res'] = {
-        --         url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Tibia/exeta_res.lua',
-        --         description = 'Script de exeta res.',
-        --         author = 'VivoDibra',
-        --         enabled = false
-        --     },
-        --     ['MW Timer'] = {
-        --         url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Tibia/mwall_timer.lua',
-        --         description = 'Script de timer de MW.',
-        --         author = 'VivoDibra',
-        --         enabled = false
-        --     },
-        --     ['Safe UE/SD'] = {
-        --         url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Tibia/safe_ue_sd.lua',
-        --         description = 'Script de safe UE/SD.',
-        --         author = 'VivoDibra',
-        --         enabled = false
-        --     },
-        --     ['Share Exp'] = {
-        --         url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Tibia/share_exp.lua',
-        --         description = 'Script de safe UE/SD.',
-        --         author = 'VivoDibra',
-        --         enabled = false
-        --     },
-        --     ['Utana Vid'] = {
-        --         url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Tibia/utana_vid.lua',
-        --         description = 'Script de utana vid.',
-        --         author = 'VivoDibra',
-        --         enabled = false
-        --     },
-        --     ['Utura'] = {
-        --         url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Tibia/utura.lua',
-        --         description = 'Script de utura.',
-        --         author = 'VivoDibra',
-        --         enabled = false
-        --     },
-
-        -- },
 
         PvP = {
             
@@ -100,12 +30,18 @@ script_manager = {
             --     author = 'VictorNeox.',
             --     enabled = false
             -- },
-            -- ['Death Counter'] = {
-            --     url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Utilities/death_counter.lua',
-            --     description = 'Script de contagem de morte.',
-            --     author = 'Lee',
-            --     enabled = false
-            -- },
+            ['Death Counter'] = {
+                url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Utilities/death_counter.lua',
+                description = 'Script de contagem de morte.',
+                author = 'Ley',
+                enabled = false
+            },
+            ['Kill Counter'] = {
+                url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Utilities/kill_count.lua',
+                description = 'Script de contagem de kills.',
+                author = 'Ley',
+                enabled = false
+            },
             ['Hide Effects'] = {
                 url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Utilities/hide_effects.lua',
                 description = 'Esconde todos os efeitos.',
@@ -114,7 +50,7 @@ script_manager = {
             },
             ['Open Main BP'] = {
                 url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Utilities/open_main_bp.lua',
-                description = 'Mantém aberta a backpack principal.',
+                description = 'Mantem aberta a backpack principal.',
                 author = 'Ley',
                 enabled = false
             },
@@ -124,18 +60,18 @@ script_manager = {
                 author = 'vBot',
                 enabled = false
             },
-            ['Use Nearby Door'] = {
+            ['Auto Door'] = {
                 url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Utilities/use_nearby_door.lua',
-                description = 'Script de usar a porta mais proxima(5 sqm).',
-                author = 'Lee',
+                description = 'Script de usar a porta mais proxima(5 sqm). [NÃO TA FUNCIONANDO]',
+                author = 'Ley',
                 enabled = false
             },
-            -- ['Porcentagem de Treino'] = {
-            --     url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Utilities/widget_train.lua',
-            --     description = 'Script de mostrar as porcentagens de treino.',
-            --     author = 'mrlthebest',
-            --     enabled = false
-            -- },
+            ['Porcentagem de Treino'] = {
+                url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Utilities/widget_train.lua',
+                description = 'Script de mostrar as porcentagens de treino.',
+                author = 'Ley',
+                enabled = false
+            },
             -- ['Alarm'] = {
             --     url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Utilities/alarm.lua',
             --     description = 'Script de alarm.',
@@ -145,6 +81,12 @@ script_manager = {
             ['Dance'] = {
                 url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Utilities/dance.lua',
                 description = 'Evita ficar AFK.',
+                author = 'Ley',
+                enabled = false
+            },
+            ['Auto Party'] = {
+                url = 'https://raw.githubusercontent.com/LeydsonW/Scripts-Community/main/Scripts/Utilities/auto_party.lua',
+                description = 'Aceita party automaticamente.',
                 author = 'Ley',
                 enabled = false
             },
